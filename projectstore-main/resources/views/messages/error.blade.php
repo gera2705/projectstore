a4cd5b2becfb7975dadefc9bcc78a0363e5e37c1
manInit <info.denisbronnikov@gmail.com>
2021-09-26 13:31:02 +0800

    удаление ошибочной копии проекта

--- projectstore-main/resources/views/messages/error.blade.php
+++
@@ -1,6 +0,0 @@
-@if(session()->has('error'))
-    <div class="success error-block closeable">
-        <div class="cross"></div>
-        <span>{{ session()->get('error') }}</span>
-    </div>
-@endif
--- projectstore-main/resources/views/messages/success.blade.php
+++
@@ -1,6 +0,0 @@
-@if(session()->has('success'))
-    <div class="success closeable">
-        <div class="cross"></div>
-        <span>{{ session()->get('success') }}</span>
-    </div>
-@endif
