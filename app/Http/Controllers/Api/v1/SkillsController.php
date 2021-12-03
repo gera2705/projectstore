1eb30a503881fcba593d4c844f7246f06f7f2476
manInit <info.denisbronnikov@gmail.com>
2021-12-03 20:01:57 +0800

    Поиск по скиллам

--- app/Http/Controllers/Api/v1/SkillsController.php
+++ app/Http/Controllers/Api/v1/SkillsController.php
@@ -9,6 +9,18 @@ class SkillsController extends Controller
 {
 	public function index() {
 		$data = Skill::all()->sortBy('id');
+
+		$searchName = $request->get('name') ?? '';
+
+		$searchName = ltrim($searchName, '"');
+		$searchName = rtrim($searchName, '"');
+
+		if ($searchName != '') {
+				$data = $data->filter(function ($value) use ($searchName) {
+						return (str_starts_with(mb_strtolower($value->tag), mb_strtolower($searchName)) !== false);
+				})->values();
+		}
+
 		return response()->json($data->toArray())->setStatusCode(200, "All skills");
 	}
 }
